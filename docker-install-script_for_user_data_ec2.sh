25a645838751eace1b584179c1cc802300cee6f1
Mor Aroesti FP <30833209+morAroesti@users.noreply.github.com>
2024-12-15 15:27:50 +0200

    Update docker-install-script_for_user_data_ec2.sh

--- docker-install-script_for_user_data_ec2.sh
+++ docker-install-script_for_user_data_ec2.sh
@@ -1,44 +1,42 @@
 #!/bin/bash
 
-apt-get update
+sudo apt-get update
 
 # Install prerequisites
-apt-get install -y ca-certificates curl gnupg
+sudo apt-get install -y ca-certificates curl gnupg
 
 # Create directory for keyrings
 install -m 0755 -d /etc/apt/keyrings
 
 # Download and install Docker's official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-chmod a+r /etc/apt/keyrings/docker.gpg
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 # Set up the repository
-echo \
+sudo echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-apt-get update
 
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Add ubuntu user to docker group to run docker without sudo
 usermod -aG docker ubuntu
 
 # Enable and start Docker service
-systemctl enable docker
-systemctl start docker
+sudo systemctl enable docker
+sudo systemctl start docker
 
 # Optional: Verify Docker installation
 docker --version
 
 
 # INSTALL GIT | CONFIG GIT | CLONE SOURCE CODE
-apt-get install -y git
+sudo apt-get install -y git
 git --version
-git config --global user.name "Mor Aroesti"
-git config --global user.email "mor.aroesti@gmail.com"
-mkdir ~/workspace
+sudo git config --global user.name "Mor Aroesti"
+sudo git config --global user.email "mor.aroesti@gmail.com"
+sudo mkdir ~/workspace
 cd ~/workspace
-git clone https://github.com/morAroesti/foodtrucks.git
-
+sudo git clone https://github.com/morAroesti/foodtrucks.git
