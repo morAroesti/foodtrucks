ccf1b8e66f18e29e808a77ec8b81f6ec45f046c8
Mor Aroesti FP <30833209+morAroesti@users.noreply.github.com>
2024-12-15 13:22:09 +0200

    Add files via upload

---
+++ docker-install-script_for_user_data_ec2.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+apt-get update
+
+# Install prerequisites
+apt-get install -y ca-certificates curl gnupg
+
+# Create directory for keyrings
+install -m 0755 -d /etc/apt/keyrings
+
+# Download and install Docker's official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Set up the repository
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Add ubuntu user to docker group to run docker without sudo
+usermod -aG docker ubuntu
+
+# Enable and start Docker service
+systemctl enable docker
+systemctl start docker
+
+# Optional: Verify Docker installation
+docker --version
+
+
+# INSTALL GIT 
+apt-get install -y git
+git --version
