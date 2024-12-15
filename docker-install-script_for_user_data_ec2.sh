25272109863e8d93969bf2af3062f9d90b65f5b5
Mor Aroesti FP <30833209+morAroesti@users.noreply.github.com>
2024-12-15 16:42:04 +0200

    Update docker-install-script_for_user_data_ec2.sh

--- docker-install-script_for_user_data_ec2.sh
+++ docker-install-script_for_user_data_ec2.sh
@@ -1,42 +1,37 @@
 #!/bin/bash
 
-sudo apt-get update
+apt-get update
 
 # Install prerequisites
-sudo apt-get install -y ca-certificates curl gnupg
+apt-get install -y ca-certificates curl gnupg git
 
 # Create directory for keyrings
 install -m 0755 -d /etc/apt/keyrings
 
 # Download and install Docker's official GPG key
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
 
 # Set up the repository
-sudo echo \
+echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+apt-get update
 
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Add ubuntu user to docker group to run docker without sudo
-sudo usermod -aG docker ubuntu
+usermod -aG docker ubuntu
 
-# Enable and start Docker service
-sudo systemctl enable docker
-sudo systemctl start docker
+systemctl enable docker
+systemctl start docker
 
-# Optional: Verify Docker installation
-docker --version
+mkdir -p /home/ubuntu/workspace
 
+cd /home/ubuntu/workspace
 
-# INSTALL GIT | CONFIG GIT | CLONE SOURCE CODE
-sudo apt-get install -y git
-git --version
-sudo git config --global user.name "Mor Aroesti"
-sudo git config --global user.email "mor.aroesti@gmail.com"
-sudo mkdir ~/workspace
-cd ~/workspace
-sudo git clone https://github.com/morAroesti/foodtrucks.git
+git clone https://github.com/morAroesti/foodtrucks.git
+
+chown -R ubuntu:ubuntu /home/ubuntu/workspace
